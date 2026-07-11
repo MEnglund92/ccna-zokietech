b9df414751a446c97d0ace54bac2cf7723ab9277
MEnglund92 <mattias.englund1992@gmail.com>
2026-07-11 23:08:41 +0200

    Fix iOS crash: remove SW skipWaiting/clients.claim, replace flatMap/flat, fix manifest start_url

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'ccna-zokietech-v4';
+const CACHE = 'ccna-zokietech-v5';
 const FILES = [
   '.',
   'index.html',
@@ -8,10 +8,10 @@ const FILES = [
   'icons/icon-512.png'
 ];
 self.addEventListener('install', e => {
-  e.waitUntil(caches.open(CACHE).then(c => c.addAll(FILES)).then(() => self.skipWaiting()));
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(FILES)));
 });
 self.addEventListener('activate', e => {
-  e.waitUntil(caches.keys().then(ks => Promise.all(ks.filter(k => k !== CACHE).map(k => caches.delete(k)))).then(() => self.clients.claim()));
+  e.waitUntil(caches.keys().then(ks => Promise.all(ks.filter(k => k !== CACHE).map(k => caches.delete(k)))));
 });
 self.addEventListener('fetch', e => {
   e.respondWith(caches.match(e.request).then(r => r || fetch(e.request).then(res => {
